2ad387b198463cc0859e7f026ece99082a22fc35
Cheng_Kevin <cheng.kaixu@iisgalvanimi.edu.it>
2026-02-06 09:51:14 +0000

    Punto 4

--- Cheng-Kevin-Fila-B/src/app/app.ts
+++ Cheng-Kevin-Fila-B/src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { SecondComponent } from './components/second-component/second-component';
+import { ThirdComponent } from './components/third-component/third-component';
+import { FourthComponent } from './components/fourth-component/fourth-component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ SecondComponent, ThirdComponent, FourthComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
