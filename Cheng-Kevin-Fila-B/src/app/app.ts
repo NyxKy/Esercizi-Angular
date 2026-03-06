e7b17f0d5d541e6abb19ddfe4c23135a83b76acf
Cheng_Kevin <cheng.kaixu@iisgalvanimi.edu.it>
2026-03-06 10:34:40 +0000

    Variabile template in Angular

--- Cheng-Kevin-Fila-B/src/app/app.ts
+++ Cheng-Kevin-Fila-B/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { SecondComponent } from './components/second-component/second-component';
 import { ThirdComponent } from './components/third-component/third-component';
 import { FourthComponent } from './components/fourth-component/fourth-component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [ SecondComponent, ThirdComponent, FourthComponent],
+  imports: [ SecondComponent, ThirdComponent, FourthComponent, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -22,6 +23,16 @@ export class App {
   }
 
   public show(): void {
-    alert("Hello World");
+    alert("Salve, " + this.showName() + " " + this.showSurn() + "!");
+  }
+
+  public show2(): void {
+    alert("Arrivederci, " + this.showName() + " " + this.showSurn() + "!");
+  }
+
+  showText = false;
+
+  show_hide(): void {
+    this.showText = !this.showText;
   }
 }
